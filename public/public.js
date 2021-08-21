94ec2a911715bf376836f6309abc5146f0d62836
yilmazme <10myilmaz@gmail.com>
2021-08-21 11:07:07 +0300

    some css fixes

--- public/public.js
+++ public/public.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const Test = document.querySelector("#test");
+
+const Reveal = document.querySelector("#reveal")
+
+Test.addEventListener("click",()=>{
+    alert("working")
+})
+
+
+window.onscroll = function(){
+    var top  = window.scrollY || document.documentElement.scrollTop;
+    if(top>500){
+      Reveal.className="reveal"; 
+    }
+    else{
+        Reveal.className="noReveal";  
+    }
+}
