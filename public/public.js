875ffd2db4b36cf284ed3a536ab40ac4468e2446
yilmazme <10myilmaz@gmail.com>
2021-08-21 19:10:54 +0300

    some css fixes

--- public/public.js
+++ public/public.js
@@ -1,18 +1,25 @@
-const Test = document.querySelector("#test");
+const Reveal = document.querySelector("#reveal");
+const Many = document.querySelectorAll(".many");
+const Resource = document.querySelectorAll(".resource");
+const Two = document.querySelectorAll(".two");
 
-const Reveal = document.querySelector("#reveal")
-
-Test.addEventListener("click",()=>{
-    alert("working")
-})
-
-
-window.onscroll = function(){
-    var top  = window.scrollY || document.documentElement.scrollTop;
-    if(top>500){
-      Reveal.className="reveal"; 
-    }
-    else{
-        Reveal.className="noReveal";  
-    }
-}
+window.onscroll = function () {
+  var top = window.scrollY || document.documentElement.scrollTop;
+  if (top < 10) {
+    Two[0].style.opacity = "1";
+  } else if (top > 500) {
+    Reveal.className = "reveal";
+    Many[0].setAttribute("id", "fade");
+    Resource[0].setAttribute("id", "fade");
+    Two[0].style.transitionDuration = "600ms";
+    Two[0].style.transitionTimingFunction = "ease-out";
+    Two[0].style.opacity = "1";
+  } else {
+    Reveal.className = "noReveal";
+    Many[0].setAttribute("id", "noFade");
+    Resource[0].setAttribute("id", "noFade");
+    Two[0].style.transitionDuration = "600ms";
+    Two[0].style.transitionTimingFunction = "ease-out";
+    Two[0].style.opacity = "0.4";
+  }
+};
